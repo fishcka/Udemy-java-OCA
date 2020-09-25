d3b32f638e12d4382a34269e4623a33786a3fc0b
ludmila-afanasenko <ludmila.afanasenko@sap.com>
2020-09-25 17:41:17 +0300

    Command line, Varargs, Foreach

---
+++ src/lesson_16/IsBlankCheck.java
@@ -0,0 +1,8 @@
+package lesson_16;
+
+public class IsBlankCheck {
+    public static void main(String[] args) {
+        String s1 = "he          llo";
+        System.out.println(s1.isBlank());
+    }
+}
